932fe10bdaa233a091086378c59368d9f324aa59
ItsLhun <nahuelghastin@gmail.com>
2021-01-04 22:11:11 +0100

    finished the functionality on command line

--- main.js
+++ main.js
@@ -1,19 +1,57 @@
-const numberOfGames = 5;
 let playerScore = 0;
-let npc = 0;
+let computerScore = 0;
+let gamesPlayed = 0;
+console.log("something");
 
-function (playerPlay, npcPlay){
-    if (normalizeString(playerPlay) === npcPlay){
-        console.log("It's a tie!");
-    }
-    if ((normalizeString(playerPlay) === "Rock") && npcPlay === "Scissors"){
-        console.log("Congratulations! ");
-        playerScore++;
-    }
+console.log(computerPlay());
+//console.log(playerPlay());
+play(computerPlay());
+
+function computerPlay() {
+  let options = ["Rock", "Paper", "Scissors"];
+  return options[randomNum(0, options.length - 1)];
 }
-let variable = prompt("input String", "");
-console.log(normalizeString(variable));
+function playerPlay() {
+  let played = prompt("Input your play!", "");
+  return normalizeString(played);
+}
+
+function play(computerPlay) {
+  while (gamesPlayed < 5) {
+    let playerPlayed = playerPlay();
+    if (playerPlayed === computerPlay) {
+      console.log("It's a tie!");
+    } else if (playerPlayed === "Rock" && computerPlay === "Scissors") {
+      console.log("You win!");
+      playerScore++;
+      gamesPlayed++;
+    } else {
+      console.log("You lose!");
+      computerScore++;
+      gamesPlayed++;
+    }
+    displayScores();
+  }
+  console.log("And the final score is:");
+  if (playerScore === computerScore) {
+    console.log("Tie!");
+  } else if (playerScore > computerScore) {
+    console.log("You win! " + playerScore + " to " + computerScore);
+  } else {
+    console.log("You lose! " + playerScore + " to " + computerScore);
+  }
+}
+
+function displayScores() {
+  console.log("Current scores:");
+  console.log("Player: " + playerScore);
+  console.log("Computer: " + computerScore);
+}
 
 function normalizeString(string) {
   return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+function randomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
